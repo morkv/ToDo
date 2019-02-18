f677618278cca3b18abc738ea72bfbba19d912f9
marheva <markhevkaroman@gmail.com>
2019-02-18 17:00:59 +0200

    Updates

---
+++ precache-manifest.679eb634df15e96b4d12a89a466297c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "910794d8b0b94bab31ce",
+    "url": "/todo/static/js/main.910794d8.chunk.js"
+  },
+  {
+    "revision": "358fe47ef65793cc73d3",
+    "url": "/todo/static/js/2.358fe47e.chunk.js"
+  },
+  {
+    "revision": "910794d8b0b94bab31ce",
+    "url": "/todo/static/css/main.4967be92.chunk.css"
+  },
+  {
+    "revision": "7329531c66cb8ae8dac902cf1cad3d7c",
+    "url": "/todo/index.html"
+  }
+];
